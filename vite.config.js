cc50434803339cc54451da7ded4a297b4fdb2e1b
TienThatTha <nhamgtien@gmail.com>
2026-04-04 22:27:27 +0700

    Fix path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/shop-acc/', // Thêm dòng này (trùng với tên kho code trên GitHub của bạn)
+  base: '/', // Thêm dòng này (trùng với tên kho code trên GitHub của bạn)
   plugins: [react()],
 })
